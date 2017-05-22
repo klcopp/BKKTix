88d405d3394bcc97bcf1a57b2b67c67a18904b5d
Maddie Lansbury <malansbury@vassar.edu>
2017-05-22 18:27:54 +0200

    Cleaned up About page

--- BKKTix/app/src/main/java/hu/ait/karen/bkktix/fragment/AboutFragment.java
+++ BKKTix/app/src/main/java/hu/ait/karen/bkktix/fragment/AboutFragment.java
@@ -4,9 +4,12 @@ package hu.ait.karen.bkktix.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import hu.ait.karen.bkktix.R;
 
@@ -19,7 +22,20 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
+        TextView tvAbout = (TextView) rootView.findViewById(R.id.tvAbout);
+        tvAbout.setText(fromHtml(getString(R.string.about)));
 
         return rootView;
+    }
+
+    @SuppressWarnings("deprecation")
+    public static Spanned fromHtml(String html){
+        Spanned result;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            result = Html.fromHtml(html,Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            result = Html.fromHtml(html);
+        }
+        return result;
     }
 }
